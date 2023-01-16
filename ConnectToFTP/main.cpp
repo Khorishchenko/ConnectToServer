19de91f2db47455fc72d803c18d939c3f15f9286
Sergii Khorishchenko <s.khorishchenko@apriorit.com>
2023-01-16 18:19:35 +0200

    Using the asio library_Added new models, first model NetCommon(static lib), second model NetClient(app).

--- ConnectToFTP/main.cpp
+++ ConnectToFTP/main.cpp
@@ -1,80 +1,149 @@
 #include <iostream>
+#include <chrono>
 #include "Login.h"
 #include "pch.h"
+#include <vector>
+
+#define ASIO_STANDALONE
+constexpr auto HOST = "93.184.216.34";
+constexpr auto HOST_NAME = "example.com";
+
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
 
 std::unique_ptr<HostPort> EnteringPortHost();
 std::unique_ptr<LoginPassword> EnteringLoginPass();
 void ReceiveAndHandelMassege(ClientSocket& sock);
 
+
+std::vector<char> vBuffer(1 * 1024);
+void GrabSomeData(asio::ip::tcp::socket& socket) {
+
+    socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
+        [&](std::error_code ec, std::size_t lenght)
+        {
+            if (!ec)
+            {
+                std::cout << "\n\n Read " << lenght << "bytes \n\n";
+
+                for (int i = 0; i < lenght; i++)
+                    std::cout << vBuffer[i];
+
+                GrabSomeData(socket);
+            }
+        }
+    );
+}
+
 int main()
 {
-    Login           login;
-    ClientSocket    sockClient;
-    char            recMessage[STRLEN];
+    Login            login;
+    asio::error_code ec;
 
-    try
+    asio::io_context context;
+    asio::io_context::work idleWork(context);
+
+    std::thread thcContext = std::thread([&] {context.run(); });
+
+    std::cout << "Enter an IP address and port of server: " << std::endl;
+    login.SetHostPort(EnteringPortHost());
+
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address(login.GetHost(), ec), login.GetPort());
+    asio::ip::tcp::socket socket(context);
+
+    socket.connect(endpoint, ec);
+    if (!ec) {
+        std::cout << " Connect " << std::endl;
+    }
+    else {
+        std::cout << " Failed to connect " << ec.message() << std::endl;
+    }
+
+    if (socket.is_open()) 
     {
-        std::cout << "Enter an IP address and port of server: " << std::endl;
-        login.SetHostPort(EnteringPortHost());
+        GrabSomeData(socket);
 
-        std::cout << "ATTEMPTING TO CONNECT..." << std::endl << std::endl;
-        sockClient.ConnectToServer(login.GetHost().c_str(), login.GetPort());
+        std::string sRequest =
+            "GET /index.html HTTPS/1.1/\r\n"
+            "Host: AprioritLLC\r\n"
+            "Connection: close\r\n\r\n";
 
-        //Recieve message from server (authentication request)
-        ReceiveAndHandelMassege(sockClient);
+        socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
 
-        std::cout << "Enter an Login and password of server: " << std::endl;
-        login.SetLoginPassword(EnteringLoginPass());
-
-        //send request to server (login)
-        sockClient.GetAndSendMessage(login.GetLogin());
-
-        //Recieve message from server (authentication request)
-        ReceiveAndHandelMassege(sockClient);
-
-        //send request to server (password)
-        sockClient.GetAndSendMessage(login.GetPassword());
-
-        //Recieve message from server (authentication request)
-        ReceiveAndHandelMassege(sockClient);
-    }
-    catch (const std::exception& ex)
-    {
-        std::cout << ex.what() << std::endl;
-        return -1;
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(2000ms);
     }
 
-    while (true) {
+    system("pause");
 
-        ReceiveAndHandelMassege(sockClient);
+    
+    //ClientSocket    sockClient;
+    //char            recMessage[STRLEN];
 
-        std::string command;
-        std::cout << "Please enter the command: ";
-        std::getline(std::cin, command);
+    //try
+    //{
+    //    std::cout << "Enter an IP address and port of server: " << std::endl;
+    //    login.SetHostPort(EnteringPortHost());
 
-        sockClient.GetAndSendMessage(command);
+    //    std::cout << "ATTEMPTING TO CONNECT..." << std::endl << std::endl;
+    //    sockClient.ConnectToServer(login.GetHost().c_str(), login.GetPort());
 
-        //server responds to selected action
-        sockClient.RecvData(recMessage, STRLEN);
+    //    //Recieve message from server (authentication request)
+    //    ReceiveAndHandelMassege(sockClient);
 
-        //if quit is entered, server sends "Goodbye"
-        std::string temp = recMessage;
-        if (temp == "INVALID ENTRY")
-        {
-            std::cout << recMessage << std::endl;
-        }
-        else if (temp == "Goodbye")
-        {
-            sockClient.CloseConnection();
-            std::cout << std::endl << "\t***DISCONNECTED" << std::endl;
-            system("pause");
-            return 0;
-        }
-        else if (temp == "List")
-        {
-            sockClient.List(recMessage);
-        }
-    }
+    //    std::cout << "Enter an Login and password of server: " << std::endl;
+    //    login.SetLoginPassword(EnteringLoginPass());
+
+    //    //send request to server (login)
+    //    sockClient.GetAndSendMessage(login.GetLogin());
+
+    //    //Recieve message from server (authentication request)
+    //    ReceiveAndHandelMassege(sockClient);
+
+    //    //send request to server (password)
+    //    sockClient.GetAndSendMessage(login.GetPassword());
+
+    //    //Recieve message from server (authentication request)
+    //    ReceiveAndHandelMassege(sockClient);
+    //}
+    //catch (const std::exception& ex)
+    //{
+    //    std::cout << ex.what() << std::endl;
+    //    return -1;
+    //}
+
+    //while (true) {
+
+    //    ReceiveAndHandelMassege(sockClient);
+
+    //    std::string command;
+    //    std::cout << "Please enter the command: ";
+    //    std::getline(std::cin, command);
+
+    //    sockClient.GetAndSendMessage(command);
+
+    //    //server responds to selected action
+    //    sockClient.RecvData(recMessage, STRLEN);
+
+    //    //if quit is entered, server sends "Goodbye"
+    //    std::string temp = recMessage;
+    //    if (temp == "INVALID ENTRY")
+    //    {
+    //        std::cout << recMessage << std::endl;
+    //    }
+    //    else if (temp == "Goodbye")
+    //    {
+    //        sockClient.CloseConnection();
+    //        std::cout << std::endl << "\t***DISCONNECTED" << std::endl;
+    //        system("pause");
+    //        return 0;
+    //    }
+    //    else if (temp == "List")
+    //    {
+    //        sockClient.List(recMessage);
+    //    }
+    //}
 
     return 0;
 }
@@ -127,7 +196,7 @@ std::unique_ptr<LoginPassword> EnteringLoginPass()
 
 void ReceiveAndHandelMassege(ClientSocket& socket)
 {
-    char recMessage[STRLEN];
+    char recMessage[STRLEN]{};
     socket.RecvData(recMessage, STRLEN);
 
     std::string temp = recMessage;
---
+++ NetClient/SimpleClient.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "olc_net.h"
+
+enum class CustomMsgTypes : uint32_t
+{
+	ServerAccept,
+	ServerDeny,
+	ServerPing,
+	MessageAll,
+	ServerMessage,
+};
+
+int main() {
+
+}
